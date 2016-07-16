d8e9a0212f44cb3f4eba2130896393648bf73c7e
Sadique Ali <sadiqalikm@gmail.com>
2016-07-16 16:40:24 -0700

    There is no need to create a connection factory.

--- src/main/java/in/sdqali/spring/redis/config/auth/RedisSessionConfig.java
+++ src/main/java/in/sdqali/spring/redis/config/auth/RedisSessionConfig.java
@@ -1,17 +1,11 @@
 package in.sdqali.spring.redis.config.auth;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @ConditionalOnProperty(name = "use.redis.session.store", havingValue = "true")
 @EnableRedisHttpSession
 public class RedisSessionConfig {
-  @Bean
-  public JedisConnectionFactory jedisConnectionFactory() {
-    return new JedisConnectionFactory();
-  }
 }
